63de89adb64278754bc00cbda6ac45a191687662
Simon Rascovsky <simonmd@gmail.com>
2013-05-27 17:33:25 -0500

    cleanup

--- js/couchctrl.js
+++ js/couchctrl.js
@@ -2,71 +2,65 @@
 google.load('visualization', '1', {packages: ['corechart']});
 
 // Begin AngularJS module
-angular.module('MRParaMetrix', ['CornerCouch','ChartFormatFilter','googlechart.directives']).
-  controller("CouchCtrl", function ($scope, $filter, cornercouch) {
+angular.module('MRParaMetrix', ['CornerCouch','googlechart.directives']).
+  controller("MainCtrl", function ($scope, cornercouch) {
 
     // Define CouchDB server
     $scope.server = cornercouch();
     // Define CouchDB database to query
     $scope.mrdb = $scope.server.getDB('mf_hash');
-    // Scanners query  - Only initial query needed
-    $scope.scanners = $scope.mrdb.query("test", "scanners", { group: true, descending: true, limit: 10 });
+    // Scanners query  - the only initial query needed
+    $scope.scanners = $scope.mrdb.query("test", "scanners", {
+                                        group: true,
+                                        descending: true,
+                                        limit: 10 });
 
-  	// Scanner change function
-    $scope.scanner_change = function(sel_scanner) {
-      console.debug("scanner_change invoked");
+    // Get studies based on selected scanner          
+    $scope.getStudies = function(sel_scanner) {
+      console.debug("getting corresponding study list...");
       $scope.studies = $scope.mrdb.query("test", "studies", {
-      																						startkey: [sel_scanner.key,0,0],
-      																						endkey: [sel_scanner.key,{},{}],
-      																						group: true});
+                                        startkey: [sel_scanner.key,0,0],
+      												          endkey: [sel_scanner.key,{},{}],
+      												          group: true});
       // Clear study model
-      $scope.cdbquery.study = null;
+      $scope.CouchQuery.study = null;
       // Clear series model
-      $scope.cdbquery.series = null;
+      $scope.CouchQuery.series = null;
     };
 
-  	// Study change function
-    $scope.study_change = function(sel_scanner,sel_study) {
-      console.debug("study_change invoked");
+  	// Get series based on selected scanner and study
+    $scope.getSeries = function(sel_scanner,sel_study) {
+      console.debug("getting corresponding series list...");
       $scope.series = $scope.mrdb.query("test", "series", {
-      																						startkey: [sel_scanner.key,sel_study[1],0],
-      																						endkey: [sel_scanner.key,sel_study[1],{}],
-      																						group: true});
+                                        startkey: [sel_scanner.key,sel_study[1],0],
+      						                      endkey: [sel_scanner.key,sel_study[1],{}],
+      						                      group: true});
       // Clear series model
-      $scope.cdbquery.series = null;
-    };
-
-    // Study change function
-    $scope.series_change = function(sel_scanner,sel_study,sel_series) {
-      console.debug("series_change invoked");
+      $scope.CouchQuery.series = null;
     };
 
   	// Submit query
-  	$scope.submitquery = function(cdbquery) {
-      console.debug("submitquery invoked");
-      console.debug("cdbquery in submitquery : %O", cdbquery);
+  	$scope.submitQuery = function(CouchQuery) {
+      console.debug("submitQuery invoked");
 
-      $scope.cdbquery.result = $scope.mrdb.list("test", "chart", "TR", {
-                                            startkey: [cdbquery.scanner.key,cdbquery.study[1],0],
-                                            endkey: [cdbquery.scanner.key,cdbquery.study[1],{}],
-                                            group: true,
-                                            limit: 10 });
-      console.debug("$scope.results in submitquery : %O", $scope.cdbquery.result);
+      $scope.CouchQuery.result = $scope.mrdb.list("test", "chart", "TR", {
+                                                  startkey: [CouchQuery.scanner.key,CouchQuery.study[1],0],
+                                                  endkey: [CouchQuery.scanner.key,CouchQuery.study[1],{}],
+                                                  group: true,
+                                                  limit: 10 });
+      console.debug("$scope.CouchQuery.result in submitquery : %O", $scope.CouchQuery.result);
 
-      // $scope.chartresults = $filter('uppercase')($scope.results); // WTF!! This works!
-
+      // Chart stuff - still not working
       var chart1 = {};
       chart1.type = "ColumnChart";
       chart1.displayed = false;
       chart1.cssStyle = "height:600px; width:100%;";
-      chart1.data = $scope.results;
-
+      chart1.data = $scope.CouchQuery.result;
       chart1.options = {};
-
       $scope.chart = chart1;
+      // End chart stuff
 
     };
-
   }
 );
 
