0d2e9c65f7bdf714c368ec82b572ec762b723361
Simon Rascovsky <simonmd@gmail.com>
2013-05-27 20:27:28 -0500

    working with watches

--- js/couchctrl.js
+++ js/couchctrl.js
@@ -9,58 +9,65 @@ angular.module('MRParaMetrix', ['CornerCouch','googlechart.directives']).
     $scope.server = cornercouch();
     // Define CouchDB database to query
     $scope.mrdb = $scope.server.getDB('mf_hash');
-    // Scanners query  - the only initial query needed
-    $scope.scanners = $scope.mrdb.query("test", "scanners", {
-                                        group: true,
-                                        descending: true,
-                                        limit: 10 });
 
-    // Get studies based on selected scanner          
-    $scope.getStudies = function(sel_scanner) {
-      console.debug("getting corresponding study list...");
-      $scope.studies = $scope.mrdb.query("test", "studies", {
-                                        startkey: [sel_scanner.key,0,0],
-      												          endkey: [sel_scanner.key,{},{}],
-      												          group: true});
-      // Clear study model
-      $scope.CouchQuery.study = null;
-      // Clear series model
-      $scope.CouchQuery.series = null;
-    };
+    // Get scanner list - the only initial query needed
+    $scope.scannerList = $scope.mrdb.query("test", "scanners", { group: true,descending: true });
+    // $scope.selectedScanner = $scope.scannerList[0];
 
-  	// Get series based on selected scanner and study
-    $scope.getSeries = function(sel_scanner,sel_study) {
-      console.debug("getting corresponding series list...");
-      $scope.series = $scope.mrdb.query("test", "series", {
-                                        startkey: [sel_scanner.key,sel_study[1],0],
-      						                      endkey: [sel_scanner.key,sel_study[1],{}],
-      						                      group: true});
-      // Clear series model
-      $scope.CouchQuery.series = null;
-    };
+    $scope.$watch('selectedScanner', function(){
+      if (!angular.isUndefined($scope.selectedScanner)) {
+        var sc = $scope.selectedScanner.key;
+        console.debug("getting corresponding study list...");
+        $scope.studyList = $scope.mrdb.query("test", "studies", {
+                                          startkey: [sc,0,0],
+                                          endkey: [sc,{},{}],
+                                          group: true});
+      }
+    });
 
-  	// Submit query
-  	$scope.submitQuery = function(CouchQuery) {
-      console.debug("submitQuery invoked");
+    $scope.$watch('selectedStudy', function(){
+      if (!angular.isUndefined($scope.selectedScanner) && !angular.isUndefined($scope.selectedStudy)) {
+        var sc = $scope.selectedScanner.key;
+        var st = $scope.selectedStudy[1];
+        console.debug("getting corresponding series list...");
+        $scope.seriesList = $scope.mrdb.query("test", "series", {
+                                startkey: [sc,st,0],
+                                endkey: [sc,st,{}],
+                                group: true});
+      }
+    });
 
-      $scope.CouchQuery.result = $scope.mrdb.list("test", "chart", "TR", {
-                                                  startkey: [CouchQuery.scanner.key,CouchQuery.study[1],0],
-                                                  endkey: [CouchQuery.scanner.key,CouchQuery.study[1],{}],
-                                                  group: true,
-                                                  limit: 10 });
-      console.debug("$scope.CouchQuery.result in submitquery : %O", $scope.CouchQuery.result);
+    $scope.$watch('selectedSeries', function(){
+      if (  !angular.isUndefined($scope.selectedScanner) &&
+            !angular.isUndefined($scope.selectedStudy) && 
+            !angular.isUndefined($scope.selectedSeries)) {
+        var sc = $scope.selectedScanner.key;
+        var st = $scope.selectedStudy[1];
+        var ss = $scope.selectedSeries[2];
+        console.debug("getting results...");
+        $scope.results = $scope.mrdb.list("test", "chart", "TR", {
+                                              startkey: [sc,st,ss,0],
+                                              endkey: [sc,st,ss,{}],
+                                              group: true});
+      }
+    });
 
-      // Chart stuff - still not working
-      var chart1 = {};
-      chart1.type = "ColumnChart";
-      chart1.displayed = false;
-      chart1.cssStyle = "height:600px; width:100%;";
-      chart1.data = $scope.CouchQuery.result;
-      chart1.options = {};
-      $scope.chart = chart1;
-      // End chart stuff
+    	// // Submit query
+     //  $scope.CouchQuery.result = $scope.mrdb.list("test", "chart", "TR", {
+     //                                              startkey: [CouchQuery.scanner.key,CouchQuery.study[1],0],
+     //                                              endkey: [CouchQuery.scanner.key,CouchQuery.study[1],{}],
+     //                                              group: true,
+     //                                              limit: 10 });
 
-    };
+      // // Chart stuff - still not working
+      // var chart1 = {};
+      // chart1.type = "ColumnChart";
+      // chart1.displayed = false;
+      // chart1.cssStyle = "height:600px; width:100%;";
+      // chart1.data = $scope.CouchQuery.result;
+      // chart1.options = {};
+      // $scope.chart = chart1;
+      // // End chart stuff
   }
 );
 
